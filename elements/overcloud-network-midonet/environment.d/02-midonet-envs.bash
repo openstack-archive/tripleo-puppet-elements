af697b8b16607ca220dc92d714d79d43e59deb5d
Jaume Devesa <devvesa@gmail.com>
2015-10-07 18:43:50 +0200

    Adding networking MidoNet element
    This element provides all the packages and the puppet midonet module to
    configure MidoNet in an OpenStack deployment.
    
    Change-Id: If08150067c89155290d531618d8c25b69d1cf879

---
+++ elements/overcloud-network-midonet/environment.d/02-midonet-envs.bash
@@ -0,0 +1,14 @@
+export DIB_MIDONET_release=${DIB_MIDONET_release:-"OSS"} # Available values OSS/MEM. To build MidoNet image with support or without it. Open Source version by default
+export DIB_MIDONET_mem_username=${DIB_MIDONET_mem_username:-} # Username of the yum repo. Only needed when DIB_MIDONET_version = "MEM"
+export DIB_MIDONET_mem_password=${DIB_MIDONET_mem_password:-} # Password of the yum repo. Only needed when DIB_MIDONET_version = "MEM"
+
+# Only source is available now for puppet modules.
+export DIB_INSTALLTYPE_ripienar_module_data='source'
+export DIB_INSTALLTYPE_puppetlabs_java='source'
+export DIB_INSTALLTYPE_puppetlabs_apt='source'
+export DIB_INSTALLTYPE_midonet_cassandra='source'
+export DIB_INSTALLTYPE_richardc_datacat='source'
+export DIB_INSTALLTYPE_deric_zookeeper='source'
+export DIB_INSTALLTYPE_nanliu_staging='source'
+export DIB_INSTALLTYPE_puppetlabs_tomcat='source'
+export DIB_INSTALLTYPE_puppet_midonet='source'
