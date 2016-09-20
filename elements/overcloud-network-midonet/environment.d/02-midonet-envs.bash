8ee01a569c6396f937d24e41d5d6f367689e7e97
Alejandro Andreu <alejandro@midokura.com>
2016-09-20 15:53:28 +0200

    Midonet element now complies with version 5.2
    This commit introduces all the necessary changes to be able to build an
    overcloud-full image with the midonet modules on it.
    
    Change-Id: I220a230868ac1f21cc2d773dff08bd9e995914fe

--- elements/overcloud-network-midonet/environment.d/02-midonet-envs.bash
+++ elements/overcloud-network-midonet/environment.d/02-midonet-envs.bash
@@ -1,14 +1,39 @@
-export DIB_MIDONET_release=${DIB_MIDONET_release:-"OSS"} # Available values OSS/MEM. To build MidoNet image with support or without it. Open Source version by default
-export DIB_MIDONET_openstack_version=${DIB_MIDONET_openstack_version:-"latest"} # OpenStack version of the midonet neutron plugin
-export DIB_MIDONET_mem_username=${DIB_MIDONET_mem_username:-} # Username of the yum repo. Only needed when DIB_MIDONET_version = "MEM"
-export DIB_MIDONET_mem_password=${DIB_MIDONET_mem_password:-} # Password of the yum repo. Only needed when DIB_MIDONET_version = "MEM"
+# Build midonet image with OSS or enterprise repository
+# Possible values: OSS, MEM
+export DIB_MIDONET_release=${DIB_MIDONET_release:-"OSS"}
 
-# Only source is available now for puppet modules.
-export DIB_INSTALLTYPE_ripienar_module_data='source'
-export DIB_INSTALLTYPE_puppetlabs_java='source'
-export DIB_INSTALLTYPE_puppetlabs_apt='source'
-export DIB_INSTALLTYPE_richardc_datacat='source'
-export DIB_INSTALLTYPE_deric_zookeeper='source'
-export DIB_INSTALLTYPE_nanliu_staging='source'
-export DIB_INSTALLTYPE_puppetlabs_tomcat='source'
-export DIB_INSTALLTYPE_puppet_midonet='source'
+# Whether the stable version should be installed or not
+# Possible values: stable, testing, unstable
+export DIB_MIDONET_stage=${DIB_MIDONET_stage:-"stable"}
+
+# Midonet version that we want to install
+export DIB_MIDONET_version=${DIB_MIDONET_version:-"5.2"}
+
+# OpenStack version of the midonet neutron plugin
+export DIB_MIDONET_openstack_version=${DIB_MIDONET_openstack_version:-"mitaka"}
+
+# Username of the yum repo (only applicable when DIB_MIDONET_release is MEM)
+export DIB_MIDONET_mem_username=${DIB_MIDONET_mem_username:-}
+
+# Password of the yum repo (only applicable when DIB_MIDONET_release is MEM)
+export DIB_MIDONET_mem_password=${DIB_MIDONET_mem_password:-}
+
+# Only download source for Puppet modules
+#export DIB_INSTALLTYPE_puppet_modules="source"
+
+export DIB_INSTALLTYPE_puppet_midonet="source"
+export DIB_INSTALLTYPE_puppetlabs_inifile="source"
+export DIB_INSTALLTYPE_puppetlabs_apt="source"
+export DIB_INSTALLTYPE_puppetlabs_java="source"
+export DIB_INSTALLTYPE_puppetlabs_stdlib="source"
+export DIB_INSTALLTYPE_puppetlabs_apache="source"
+export DIB_INSTALLTYPE_puppetlabs_concat="source"
+export DIB_INSTALLTYPE_zookeeper="source"
+export DIB_INSTALLTYPE_cassandra="source"
+export DIB_INSTALLTYPE_puppet_curator="source"
+export DIB_INSTALLTYPE_puppet_elasticsearch="source"
+export DIB_INSTALLTYPE_puppet_logstash="source"
+export DIB_INSTALLTYPE_puppet_lib_file_concat="source"
+export DIB_INSTALLTYPE_puppet_datacat="source"
+export DIB_INSTALLTYPE_puppet_midonet_openstack="source"
+
