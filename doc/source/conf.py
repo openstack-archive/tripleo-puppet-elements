0df6e75da7394bf2e0ded416ceef1277ebfbf347
Emilien Macchi <emilien@redhat.com>
2017-06-27 16:30:55 -0400

    Switch from oslosphinx to openstackdocstheme
    As part of the docs migration work[0] for Pike we need to switch to use the
    openstackdocstheme.
    
    [0] https://review.openstack.org/#/c/472275/
    
    Change-Id: Iffe8047944add2d8f63cb8031a633c1fe8fff09c

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['oslosphinx']
+extensions = ['openstackdocstheme']
 templates_path = ['_templates']
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -86,7 +86,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -238,3 +238,8 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
+
+# openstackdocstheme options
+repository_name = 'openstack/tripleo-puppet-elements'
+bug_project = 'tripleo'
+bug_tag = 'documentation'
