8582a2941804cdc7a89c0c12e0b16a4638b167aa
Emilien Macchi <emilien@redhat.com>
2017-08-30 14:32:18 -0700

    Use Python to compute release notes version
    Leave the version fields blank, since the release notes document
    applies to all versions.
    That will avoid manual changes in the future like we did until now.
    
    Change-Id: Ic5b7a7043fc9897626cf33487f589a610801b17c
    (cherry picked from commit f23ddd8b1498b32894b8c5939907b8d7be76d7fb)

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -52,9 +52,9 @@ copyright = u'2016, TripleO Developers'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '6.2.0'
+release = ''
 # The short X.Y version.
-version = '6.2.0'
+version = ''
 
 # The full version, including alpha/beta/rc tags.
 
