9f4007d65d4359db00055fad5bcd83c8f3946ec1
Takashi Kajinami <tkajinam@redhat.com>
2022-08-10 00:00:35 +0900

    Remove puppet-auditd
    ... because it is no longer used.
    
    Depends-on: https://review.opendev.org/852598
    Change-Id: I1a4b53c1ecb015fdc16625408d376b0be85ecee4

--- elements/puppet-modules/environment.d/02-puppet-modules-install-types.sh
+++ elements/puppet-modules/environment.d/02-puppet-modules-install-types.sh
@@ -5,7 +5,6 @@ if [ "$DIB_INSTALLTYPE_puppet_modules" = "source" ]; then
   for module_name in \
     puppetlabs-apache \
     puppet-aodh \
-    puppet-auditd \
     puppet-barbican \
     puppet-ceilometer \
     puppet-gnocchi \
